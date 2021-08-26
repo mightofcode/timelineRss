3661bd7704ca11180857eabbe2385297de6083e3
root <root@localhost.localdomain>
2021-08-26 18:07:28 +0800

    fix close click

--- client/src/components/articleWindow/index.js
+++ client/src/components/articleWindow/index.js
@@ -1,17 +1,23 @@
 import React, {useState, useEffect} from 'react';
 
 import './index.less';
-import { history } from 'umi';
+import {history} from 'umi';
 import ArticleBoard from "./articleBoard";
 import CloseBtn from "./closeBtn";
-export default function ArticleWindow({handleClose,article}) {
+
+export default function ArticleWindow({handleClose, article}) {
 
   useEffect(() => {
 
   }, []);
 
   return (
-    <div className={"ArticleWindow"} >
+    <div className={"ArticleWindow"} onClick={(e) => {
+      e.preventDefault();
+      if (e.target === e.currentTarget) {
+        handleClose();
+      }
+    }}>
       <CloseBtn handleClose={handleClose}/>
       <ArticleBoard article={article}/>
     </div>
