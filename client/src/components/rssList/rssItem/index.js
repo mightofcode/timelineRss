04e990f0cc0e6643a6207eb236fe05c3c4770598
root <root@localhost.localdomain>
2021-08-28 15:28:41 +0800

    fix rss list style

--- client/src/components/rssList/rssItem/index.js
+++ client/src/components/rssList/rssItem/index.js
@@ -8,10 +8,16 @@ export default function RssItem({rss}) {
 
   }, []);
 
+  let shortTitle=rss?.name||"";
+  if(shortTitle.length>13){
+    shortTitle=shortTitle.substring(0,11)+"...";
+  }
+
+
   return (
     <div className={"RssItem"}>
       <img className={"rssIcon"} src={rss?.icon}/>
-      <div className={"title"}>{rss?.name}</div>
+      <div className={"title"}>{shortTitle}</div>
       {/*<div className={"count"} >180</div>*/}
     </div>
   )
